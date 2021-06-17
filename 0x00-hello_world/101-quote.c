661ec492b09446ee2ee5d58ef56e1b4decc49efa
4ndr3sxy <andrescastanedaxy@gmail.com>
2021-06-17 16:17:03 -0700

    [ADD] - creation task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - initial function
+ *
+ * Return: 1 to error
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 62);
+return (1);
+}
