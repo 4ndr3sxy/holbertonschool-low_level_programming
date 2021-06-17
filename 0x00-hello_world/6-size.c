8971e3808182f8de00d0faf142cf1695338babbf
4ndr3sxy <andrescastanedaxy@gmail.com>
2021-06-17 14:23:06 -0700

    [FIX] - Fix code task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,12 @@ int intT;
 float floatT;
 char charT;
 long int longIT;
+long double longDT;
 
 printf("Size of a char: %ld byte(s)\n", sizeof(charT));
 printf("Size of an int: %ld byte(s)\n", sizeof(intT));
 printf("Size of a long int: %ld byte(s)\n", sizeof(longIT));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(longIT));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(longDT));
 printf("Size of a float: %ld byte(s)\n", sizeof(floatT));
 return (0);
 }
