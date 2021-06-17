373dbc82a8e524463441c084eb62dbb8810faa92
4ndr3sxy <andrescastanedaxy@gmail.com>
2021-06-17 14:20:56 -0700

    [ADD] - creation code task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - init function
+ *Description: print size of type of data
+ *Return: 0 to success
+ */
+int main(void)
+{
+int intT;
+float floatT;
+char charT;
+long int longIT;
+
+printf("Size of a char: %ld byte(s)\n", sizeof(charT));
+printf("Size of an int: %ld byte(s)\n", sizeof(intT));
+printf("Size of a long int: %ld byte(s)\n", sizeof(longIT));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(longIT));
+printf("Size of a float: %ld byte(s)\n", sizeof(floatT));
+return (0);
+}
