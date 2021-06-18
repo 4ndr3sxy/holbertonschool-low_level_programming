cfc623c500da64306096486bc5f8401f9d21996c
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-18 18:34:43 -0500

    [ADD] - Creation code to task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Initial function
+ *
+ *Return: return 0 to success
+ */
+int main(void)
+{
+	int num = 48;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		putchar(num == 57 ? '$' : ',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
