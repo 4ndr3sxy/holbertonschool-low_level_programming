72b5c498e5e105cbe5ca2cb6148e7592250e912a
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-18 18:12:45 -0500

    [ADD] - Creation code tot ask 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Initial function
+ *
+ *Return: return 0 to success
+ */
+int main(void)
+{
+	int ch =48;
+
+	for (ch = 48; ch < 58; ch++)
+		putchar(ch);
+	for (ch = 97;ch < 103;ch++)
+		putchar(ch);
+	return (0);
+}
