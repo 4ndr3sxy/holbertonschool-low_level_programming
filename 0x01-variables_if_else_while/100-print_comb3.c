64df67f575f8585c4fffafeca33063f6caa42d5f
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-18 20:32:12 -0500

    [ADD] - Creation code to task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Initial function
+ *
+ *Return: return 0 to success
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i; j < 58; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(i == 56 && j == 57 ? '\n' : ',');
+				if (i != 56 || j != 57)
+					putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
