df28e4adb795a8ea1e919a1722058b4a90d7b8b4
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-21 10:32:17 -0500

    [ADD] - creation code to task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - called in main()
+ * Description: show/print alphabet from a function
+ * Return: return 0 to success
+ */
+int print_alphabet(void)
+{
+	char ch = 97;
+
+	for (ch = 97; ch < 123; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
+/**
+ * main - Initial function
+ * Description: show/print alphabet from a function
+ * Return: return 0 to success
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
