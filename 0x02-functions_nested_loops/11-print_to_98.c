84ff94d93dd53c211042ddbf951312543420db49
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-22 12:43:40 -0500

    [ADD] - Creation code and file task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * print_to_98 - called by main()
+ * Description: print all numers naturals between n to 98
+ * @n: initial number
+ * Return: without return
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
