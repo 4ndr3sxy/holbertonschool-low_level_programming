38182359c59d1e16f474d2ffa234ecad858e8f9f
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-22 12:02:54 -0500

    [DEL] - function main() deleted

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -24,35 +24,3 @@ int print_sign(int n)
 		return (-1);
 	}
 }
-
-/**
- * main - Initial function
- * Description: call a function print_sign()
- * Return: return 0 to success
- */
-int main(void)
-{
-	int r;
-
-	r = print_sign(98);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(0);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(0xff);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(-1);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
