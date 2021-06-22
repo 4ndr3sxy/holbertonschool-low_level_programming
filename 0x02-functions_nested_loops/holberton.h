e34aeb28f4e1cd57c5f76fbe7a877ca2bfd960eb
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-22 10:07:32 -0500

    [ADD] - files holberton.h and _putchar.c added

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,38 @@
+/* _putchar - print a character */
+int _putchar(char c);
+
+/* print_alphabet - print those characters */
+void print_alphabet(void);
+
+/* print_alphabet - print those characters ten times */
+void print_alphabet_x10(void);
+
+/* _islower - print character lower */
+int _islower(int c);
+
+/* _isalpha - print character alpha */
+int _isalpha(int c);
+
+/* print_sign - print number positive, negative or zero */
+int print_sign(int n);
+
+/* _abs - print absolute value to any number */
+int _abs(int);
+
+/* print_last_digit - print last digit of any number */
+int print_last_digit(int);
+
+/* jack_bauer - print every minute of the day */
+void jack_bauer(void);
+
+/* times_table - print tables between 0 in 9 */
+void times_table(void);
+
+/* add - sum between two numbers */
+int add(int, int);
+
+/* print_to_98 - print every number between n to 98 */
+void print_to_98(int n);
+
+/* print_times_table - print n times tables, between n in n */
+void print_times_table(int n);
