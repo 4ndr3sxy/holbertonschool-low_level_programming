49a98f8ab789e244912537e0595da66a370a0976
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-22 12:38:45 -0500

    [ADD] - Creation file and code task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * add - Initial function
+ * Description: addition between two numbers
+ * Return: return sum of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
