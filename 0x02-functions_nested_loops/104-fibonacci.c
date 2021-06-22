a110d052c4aa467108c55ba7ab3d07541b4af3db
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-22 16:51:52 -0500

    [ADD] - Creation code and file task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * fibonacci - caleld by main()
+ * Return: without return
+ */
+void fibonacci(void)
+{
+	int i;
+	long num1 = 0;
+	long num2 = 1;
+	long tmpResult;
+
+	for (i = 1; i <= 98; i++)
+	{
+		tmpResult = num1 + num2;
+
+		printf("%lld", tmpResult);
+		putchar(i != 98 ? ',' : '\n');
+		if (i != 98)
+		{
+			putchar(' ');
+		}
+		num1 = num2;
+		num2 = tmpResult;
+	}
+}
+
+/**
+ * main - Initial function
+ * Return: return 0 to success
+ */
+int main(void)
+{
+	fibonacci();
+	return (0);
+}
