3760079994d56ee29821a7f67f8e8f9348d67cf7
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-22 15:06:41 -0500

    [ADD] - Creation of code and files task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * sum_naturals - called by main()
+ * Description: sumatory of number natural multiples of 3 or 5
+ * Return: without return
+ */
+void sum_naturals(void)
+{
+	int i;
+	int sumMultiples = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sumMultiples += i;
+		}
+	}
+	printf("%d\n", sumMultiples);
+}
