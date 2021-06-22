841ff3005a2748d673cc4975fa970521337a8e90
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-22 12:33:21 -0500

    [ADD] - Creation code and file task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_last_digit - called by main()
+ * Description: Convert absolute value to any number
+ * @n: number to validate
+ * Return: return 0 to success
+ */
+int print_last_digit(int n)
+{
+	n %= 10;
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	_putchar(n + '0');
+	return (n);
+}
