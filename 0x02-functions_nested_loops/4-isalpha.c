329b499024cfcc382f4c584b75858ad404973acb
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-23 09:42:45 -0500

    [FIX] - Fix return to function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  * @ch: Character to validate
  * Return: return 0 to success
  */
-char _isalpha(char ch)
+int _isalpha(char ch)
 {
 	if ((ch >=  97 && ch <= 122) || (ch >=  65 && ch <= 90))
 		return (1);
