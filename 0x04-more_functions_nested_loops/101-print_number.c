61cc763ea0a7f025a1c06f791f27406102bc8ab0
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-24 16:34:01 -0500

    [FIX] - Fix code in files of task 11,12 and holberton.h

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * print_number - called to main()
@@ -11,33 +11,33 @@ void print_number(int n)
 	{
 		if (n < 10)
 		{
-			putchar(n + '0');
+			_putchar(n + '0');
 		}
 		else if (n < 100)
 		{
-			putchar(n / 10 + '0');
-			putchar(n % 10 + '0');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
 		}
 		else if (n < 1000)
 		{
-			putchar(n / 100 + '0');
-			putchar(((n / 10) % 10) + '0');
-			putchar(n % 10 + '0');
+			_putchar(n / 100 + '0');
+			_putchar(((n / 10) % 10) + '0');
+			_putchar(n % 10 + '0');
 		}
 		else
 		{
-			putchar(n / 1000 + '0');
-			putchar(((n / 100) % 10) + '0');
-			putchar(((n / 10) % 10) + '0');
-			putchar(n % 10 + '0');
+			_putchar(n / 1000 + '0');
+			_putchar(((n / 100) % 10) + '0');
+			_putchar(((n / 10) % 10) + '0');
+			_putchar(n % 10 + '0');
 
 		}
 	}
 	else
 	{
 		n *= -1;
-		putchar('-');
-		putchar(n / 10 + '0');
-		putchar(n % 10 + '0');
+		_putchar('-');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 }
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -25,6 +25,8 @@ void print_triangle(int size);
 
 void print_number(int n);
 
+void prime_factor(void);
+
 int _putchar(char c);
 
 #endif /* HOLBERTON_H */
