f5995ad9b5fd04cac0240b43aac64ecdbab9537d
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-24 13:53:24 -0500

    [ADD] - Creation file and code task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - called to main()
+ * Description: multiply between two numbers
+ * @num1: number 1 a multiply
+ * @num2: number 2 a multiply
+ * Return: return multiply between two numbers
+ */
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
