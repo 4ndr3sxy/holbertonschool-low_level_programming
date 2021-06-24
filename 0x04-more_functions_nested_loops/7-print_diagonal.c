d86ce5ba54324d4afdc391a483aac06e2811bddc
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-24 14:31:07 -0500

    [ADD] - Creation file and code task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print diagonal with character '\' n times
+ * @n: print n times a character
+ * Return: without return
+ */
+void print_diagonal(int n)
+{
+	int m = 0;
+	int l = 0;
+
+	while (m < n)
+	{
+		while (l < m)
+		{
+			_putchar(' ');
+			l++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		l = 0;
+		m++;
+	}
+	if (n == 0)
+		_putchar('\n');
+}
