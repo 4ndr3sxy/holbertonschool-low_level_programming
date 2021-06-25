31fb14449cec3c6e11514a68cd897948db056a0b
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-25 12:02:59 -0500

    [FIX] - Fix code task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <limits.h>
 
 /**
  * print_number - called to main()
@@ -7,12 +8,18 @@
  */
 void print_number(int n)
 {
-	int arr[sizeof(int) * 16];
+	int n1 = n;
+	int arr[sizeof(int) * 8];
 	int i = 0;
 	int j, r;
 
-	if (n > 0)
+	if (n != 0)
 	{
+		if (n < 0)
+		{
+			n *= -1;
+			putchar('-');
+		}
 		while (n != 0)
 		{
 			r = n % 10;
@@ -22,27 +29,20 @@ void print_number(int n)
 		}
 		for (j = i - 1; j > -1; j--)
 		{
-			_putchar(arr[j] + '0');
+			if (n1 < -2147483647)
+			{
+				int val = arr[j] * -1;
+
+				putchar(val + '0');
+			}
+			else
+			{
+				putchar(arr[j] + '0');
+			}
 		}
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
 	}
 	else
 	{
-		n *= -1;
-		_putchar('-');
-		while (n != 0)
-		{
-			r = n % 10;
-			arr[i] = r;
-			i++;
-			n = n / 10;
-		}
-		for (j = i - 1; j > -1; j--)
-		{
-			_putchar(arr[j] + '0');
-		}
+		putchar('0');
 	}
 }
