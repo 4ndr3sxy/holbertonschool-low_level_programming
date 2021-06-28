d89ba787bf2315b183f2e4bf547bca811855d01a
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-28 17:54:37 -0500

    [ADD] - Creation code and file task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_array - print n positions of array
+ * @a: array
+ * @n: amount of positions to print
+ * Return: wirhout return
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i + 1 != n)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
+		}
+	}
+}
