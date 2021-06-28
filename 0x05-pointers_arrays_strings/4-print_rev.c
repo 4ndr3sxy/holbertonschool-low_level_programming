f09baaab6aadcdda6e04ced52fff07a75213ec69
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-28 14:15:52 -0500

    [ADD] - Creation code and file task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_rev - Initial function
+ * @s: char to print in revert
+ * Return: return 0 to success
+ */
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+	char *m = s;
+
+	while (*m != 0)
+	{
+		count++;
+		m++;
+	}
+	for (i = count; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
