d9fa1ee0ec455a5d5123db260f4cdd59cc70a8f6
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-28 17:26:01 -0500

    [FIX] - Fix code task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,12 +7,27 @@
  */
 void puts_half(char *str)
 {
+	char *str1 = str;
 	int i = 0;
+	int count = 0;
 
-	while (*str != 0)
+	while (*str1 != '\0')
 	{
+		count++;
+		str1++;
+	}
+	if (count % 2 == 0)
+	{
+		count /= 2;
+	}
+	else
+	{
+		count = (count - 1) / 2;
+	}
 
-		if (i % 2 != 0)
+	while (*str != '\0')
+	{
+		if (i >= count)
 		{
 			putchar(*str);
 		}
