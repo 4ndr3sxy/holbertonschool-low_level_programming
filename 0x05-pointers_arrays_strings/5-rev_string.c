ae6b42800a6d7cf33f96b93744140433ea83565b
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-06-28 19:30:32 -0500

    [FIX] - Fix code in logical

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,15 +10,24 @@ void rev_string(char *s)
 	int i;
 	int count = 0;
 	char *m = s;
+	char *n;
 
 	while (*m != 0)
 	{
 		count++;
 		m++;
 	}
+	int j = 0;
 	for (i = count - 1; i >= 0; i--)
 	{
-		_putchar(s[i]);
+		n[j] = s[i];
+		j++;
 	}
-	_putchar('\n');
+	j = 0;
+	while (*s != '\0')
+	{
+		*s = n[j];
+		j++;
+		s++;
+	}
 }
