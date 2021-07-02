1bb7223fee6bf1e59806ff4c833f5a85977a9e97
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-02 11:25:33 -0500

    [FIX] - Fix of Return ()

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,26 +11,15 @@ int _strcmp(char *s1, char *s2)
 	int val1 = 0, val2 = 0;
 	int i, result;
 
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 1; i++)
 	{
 		val1 += s1[i];
 	}
-	for (i = 0; i < 6; i++)
+	for (i = 0; i < 1; i++)
 	{
 		val2 += s2[i];
 	}
 	result = val1 - val2;
 
-	if (result == 0)
-	{
-		return (0);
-	}
-	else if (result > 0)
-	{
-		return (15);
-	}
-	else
-	{
-		return (15 * -1);
-	}
+	return (result);
 }
