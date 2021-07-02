24df1e97c5a0b0b0332d63bf217c4486c797c90b
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-02 11:12:49 -0500

    [ADD] - Creation code and file task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copy one string in other
+ * @dest: chain initial
+ * @src: chain to copy in *dest
+ * @n: count of letter to add a *dest
+ * Return: return pointer a dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	size_t j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	if (j < n)
+		dest[j] = '\0';
+	return (dest);
+}
