34693b6da3510038dc3e3681326ac27632981b2d
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-02 14:52:13 -0500

    [FIX] - test

--- 0x06-pointers_arrays_strings/101_print_number.c
+++ 0x06-pointers_arrays_strings/101_print_number.c
@@ -7,35 +7,5 @@
  */
 void print_number(int n)
 {
-	int n1 = n;
-	int i = 0;
-	int j, r;
 
-	if (n != 0)
-	{
-		if (n < 0)
-		{
-			n *= -1;
-			_putchar('-');
-		}
-		while (n != 0)
-		{
-			r = n % 10;
-			i++;
-			n = n / 10;
-		}
-		for (j = i - 1; j > -1; j--)
-		{
-			if (n1 < -2147483647)
-			{
-				int val = -1;
-
-				_putchar(val + '0');
-			}
-		}
-	}
-	else
-	{
-		_putchar('0');
-	}
 }
