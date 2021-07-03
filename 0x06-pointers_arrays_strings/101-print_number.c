6797557a47aead948ce6e99698724ce7c4b7ab58
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-03 17:26:59 -0500

    [FIX] - Fix code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,5 +7,39 @@
  */
 void print_number(int n)
 {
-	_putchar(n);
+	int i, j;
+	int cpN = n;
+	int numberDigits = 0;
+	int cpNumberDigits;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+
+	do {
+		numberDigits++;
+		cpN /= 10;
+	} while (cpN != 0);
+	cpNumberDigits = numberDigits;
+
+	for (i = 0; i < cpNumberDigits; i++)
+	{
+		cpN = n;
+		for (j = 0; j < cpNumberDigits; j++)
+		{
+			if (j + 1 < numberDigits)
+			{
+				cpN /= 10;
+			}
+			else
+			{
+				cpN %= 10;
+			}
+		}
+		_putchar(cpN + '0');
+		numberDigits--;
+	}
 }
+
