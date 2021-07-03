fa26dd51ec1b252827bff0b7bb5bcb5fc603ab3f
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-03 18:21:06 -0500

    [ADD] - Creation code and file task 12

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_buffer - function called to main()
+ * @b: char
+ * @size: int
+ * Return: withour return
+ */
+void print_buffer(char *b, int size)
+{
+	if (*b)
+	{
+
+	}
+	if (size)
+	{
+
+	}
+}
