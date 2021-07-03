400c5dcf74150703365bdbdb74714f8e1e4f5c67
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-02 19:03:51 -0500

    [ADD] - Creation code and file task 11

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * infinite_add - function
+ * @n1: char 1
+ * @n2: char 2
+ * @r: char 3
+ * @size_r: int 1
+ * Return: char
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return ('0');
+}
