f48f79cf82f65268c8d30c3810a315f40dc28272
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-04 18:05:58 -0500

    [ADD] - Creation code and file task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,21 +1,30 @@
-#include "holberton.h"
-
-/**
- * keygen - Initial function
- * Return: Without return
- */
-void keygen(void)
-{
-	_putchar('\n');
-}
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Initial function
+ * Description: Generate Keygen
  * Return: return 0 to success
  */
 int main(void)
 {
-	keygen();
+	char *array = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	int count = 2772;
+	srand(time(0));
+	while (count)
+	{
+		int numRand = ( rand() % 63);
+		char charRand = array[numRand];
+		count -= charRand;
+		printf("%c", charRand);
+		if (count <= 122)
+		{
+			charRand = count;
+			printf("%c", charRand);
+			count -= charRand;
+		}
+	}
+	printf("\n");
 	return (0);
 }
