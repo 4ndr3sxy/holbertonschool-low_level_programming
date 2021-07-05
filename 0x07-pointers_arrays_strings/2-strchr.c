837770875ab0029e921a1db7ec758cbdb5c3a2e3
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-05 15:11:24 -0500

    [ADD] - Creation code and file task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _memset - change values in buffer
+ * @s: chain of characters (buffer)
+ * @c: char to search
+ * Return: pointer s
+ */
+char *_strchr(char *s, char c)
+{
+	int i, count = 0;
+	char *result;
+
+	while (s[count])
+	{
+		count++;
+	}
+
+	for (i = 0; i < count; i++)
+	{
+		s++;
+		if (s[i] == c)
+		{
+			result = s;
+			break;
+		}
+	}
+	return (result);
+}
