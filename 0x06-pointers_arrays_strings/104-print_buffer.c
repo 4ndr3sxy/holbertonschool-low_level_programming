bdf0f687cb4f0470b84151944696d88e28598f1b
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-06 20:30:54 -0500

    [FIX] - Fix code

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,12 +8,41 @@
  */
 void print_buffer(char *b, int size)
 {
-	if (*b)
-	{
+	int hex = 0;
+	int i, j, k;
+	int val = 0;
 
-	}
-	if (size)
+	for (i = 0; i <= size;)
 	{
-
+		printf("%08x: ", hex);
+		for (j = i; j < (i + 10); j++)
+		{
+			if (j >= 126)
+			{
+				printf("     ");
+			}
+			else
+			{
+				printf("%02x%02x ", b[j], b[j + 1]);
+			}
+			j++;
+		}
+		for (k = i; k < (i + 10); k++)
+		{
+			if ((b[k] >= 0 && b[k] <= 9) ||  b[k] == '\n' || (b[k] == '\0' && k != 127))
+			{
+				if (b[k] == '\0' && k == 126)
+				{
+					break;
+				}
+				val = 1;
+			}
+			printf("%c", (val ? '.' : b[k]));
+			val = 0;
+		}
+		printf("\n");
+		i += 10;
+		hex += 10;
 	}
+	printf("\n");
 }
