8acfa7c9748ac9d4baa6b4f1b17e19b660a5e8f5
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-08 13:36:33 -0500

    [ADD] - Creation code and file task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *main - Initial function
+ *@argc: number of command line arguments
+ *@argv: array containing the program command line arguments
+ *Return: 0 to success;
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+		int result = num1 * num2;
+
+		printf("%d\n", result);
+		return (0);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+}
