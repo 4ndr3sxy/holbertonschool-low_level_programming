479afeb0c05913c3425d7d3f09a28b90e4082728
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-09 23:25:50 -0500

    [ADD] - Creation code and file task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse with recursivity
+ * @s: chain of characters to print
+ * Return: Without return
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar(*s);
+	}
+	else
+	{
+		s++;
+		_print_rev_recursion(s);
+		putchar(*s);
+	}
+	s--;
+	s--;
+	if (*s == '\0')
+	{
+		s++;
+		putchar(*s);
+	}
+	else
+	{
+		s++;
+		s++;
+	}
+}
