0bafd46bc83d235a4d06d2bf0f172982efd7a676
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-13 14:10:47 -0500

    [FIX] - Fix code of size

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *stringMalloc;
 	unsigned int i = 0;
 
-	stringMalloc = malloc(sizeof(unsigned int)  * size);
+	stringMalloc = malloc(sizeof(unsigned int)  * (size + 1));
 
 	if (stringMalloc == NULL || size == 0)
 	{
