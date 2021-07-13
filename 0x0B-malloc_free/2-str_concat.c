8c704dc16eea0ea8ffdba0dd7468a17b1bd0ed1b
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-13 16:32:46 -0500

    [FIX] - Fix code in values 'NULL'

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,35 +9,41 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i = 0, j = 0;
-	char *stringMalloc;
+	char *stringMalloc = "";
 
-	while (s1[i])
-	{
-		i++;
-	}
-	while (s2[j])
-	{
-		j++;
-	}
+	if (s1)
+		while (s1[i])
+		{
+			i++;
+		}
+	if (s2)
+		while (s2[j])
+		{
+			j++;
+		}
+
 	i += j;
-	stringMalloc = malloc(sizeof(char) * i);
+	stringMalloc = malloc(sizeof(char) * i + 1);
 	if (stringMalloc == NULL)
 	{
 		return (NULL);
 	}
 	i = 0;
-	while (s1[i])
-	{
-		stringMalloc[i] = s1[i];
-		i++;
-	}
+	if (s1)
+		while (s1[i])
+		{
+			stringMalloc[i] = s1[i];
+			i++;
+		}
 	j = 0;
-	while (s2[j])
-	{
-		stringMalloc[i] = s2[j];
-		i++;
-		j++;
-	}
+	if (s2)
+		while (s2[j])
+		{
+			stringMalloc[i] = s2[j];
+			i++;
+			j++;
+		}
 	stringMalloc[i] = '\0';
+
 	return (stringMalloc);
 }
