1ecad0783c05bf83dbf22e83288902ac47dfb94b
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-14 11:16:33 -0500

    [FIX] - Fix size in malloc()

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,14 +9,14 @@
 int **alloc_grid(int width, int height)
 {
 	int i = 0, j = 0;
-	int **pointToPointArray2d = malloc(sizeof(int *) * height + 1);
+	int **pointToPointArray2d = malloc(sizeof(int *) * height);
 
 	if (pointToPointArray2d == NULL)
 		return (NULL);
 	if (width < 1 || height < 1)
 		return (NULL);
 	for (i = 0; i < height; i++)
-		pointToPointArray2d[i] = malloc(sizeof(int) * width + 1);
+		pointToPointArray2d[i] = malloc(sizeof(int) * width);
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			pointToPointArray2d[i][j] = 0;
