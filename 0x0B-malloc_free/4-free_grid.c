41d0049a4530b80a4351c71546602bd72bdf5cdd
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-13 21:16:36 -0500

    [ADD] - Creation code and file task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * free_grid - created new array using malloc()
+ * @grid: array int**
+ * @height: rows of array 2d
+ * Return: int** with new array 2d
+ */
+void free_grid(int **grid, int height)
+{
+}
