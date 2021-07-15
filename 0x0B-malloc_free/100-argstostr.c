43c0cd4e239ee2259b1a477a699aef6f43915c7c
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-15 11:02:16 -0500

    [ADD] - Creation code and file task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,70 +1,29 @@
 #include "holberton.h"
 
+/**
+ * argstostr - Args with \n in a char *
+ * @ac: count og args
+ * @av: pointer to pinter of args
+ * Return: Chain of characters with \n
+ */
 char *argstostr(int ac, char **av)
 {
-    int i, j, k;
-    char concatArgs[100];
-    /*
-    printf("%d\n",ac);
-    printf("%s\n",*av);
-    printf("%c\n",*av[0]);
-    *av++;
-    printf("%s\n",*av);
-    printf("%c\n",*av[0]);
-    */
-   printf("|%c|",av[1][4]);
-    for (i = 1; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-        {
-            printf("-%c-",av[i][j]);
-        }
-        printf("\n");
+	int i, j, k = 0;
+	char *concatArgs = malloc(sizeof(char) * 100);
 
-        // printf("-%c-",*av[2]);
-        // printf("-%c-",*av[3]);
-        // printf("-%c-",*av[4]);
-        
-        /*
-        for (j = 0; *av+j; j++)
-        {
-            if ((*av[j] >= 65 && *av[j] <= 90) || (*av[j] >= 97 && *av[j] <= 122))
-            {
-                concatArgs[k] = *av[j];
-            }
-            else{
-                concatArgs[k] = '\n';
-            }
-            k++;   
-        }
-        */
-    }
-
-    /*
-    char *newChain;
-    int i;
-
-    if (ac - 1 == 0 || av == NULL)
-        return (NULL);
-    for (i = 0; i < ac; i++)
-    {
-        *newChain = newChain + *av[ac];
-    }
-    return (newChain);    
-    */
-   return (NULL);
-}
-
-int main(int ac, char *av[])
-{
-    char *s;
-
-    s = argstostr(ac, av);
-    if (s == NULL)
-    {
-        return (1);
-    }
-    printf("%s", s);
-    free(s);
-    return (0);
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			if ((av[i][j] >= 65 && av[i][j] <= 90) ||
+			(av[i][j] >= 97 && av[i][j] <= 122))
+			{
+				concatArgs[k] = av[i][j];
+				k++;
+			}
+		}
+		concatArgs[k] = '\n';
+		k++;
+	}
+	return (concatArgs);
 }
