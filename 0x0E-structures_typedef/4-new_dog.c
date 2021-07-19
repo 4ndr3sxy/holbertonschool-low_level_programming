a1bd6e16a25c38e952b55cbdc0703f27363ac1bf
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-19 18:50:15 -0500

    [FIX] - Fix count characters

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,35 +9,39 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0, j = 0;
+	char *cpName, *cpOwner;
 	dog_t *ptrNew_dog = malloc(sizeof(dog_t));
 
 	if (ptrNew_dog == NULL)
+		return (NULL);
+	for (i = 0; name[i]; i++)
+	{
+	}
+	for (j = 0; j < owner[j]; j++)
+	{
+	}
+	cpName = malloc(sizeof(char) * (i + 1));
+	if (cpName == NULL)
 	{
 		free(ptrNew_dog);
 		return (NULL);
 	}
-
-	if (name == NULL)
+	cpOwner = malloc(sizeof(char) * (j + 1));
+	if (cpOwner == NULL)
 	{
+		free(cpName);
 		free(ptrNew_dog);
 		return (NULL);
 	}
-	else
-	{
-		(*ptrNew_dog).name = name;
-	}
-
+	for (i = 0; name[i]; i++)
+		cpName[i] = name[i];
+	cpName[i] = '\0';
+	for (j = 0; owner[j]; j++)
+		cpOwner[j] = owner[j];
+	cpOwner[j] = '\0';
+	(*ptrNew_dog).name = cpName;
 	(*ptrNew_dog).age = age;
-
-	if (owner == NULL)
-	{
-		free(ptrNew_dog);
-		return (NULL);
-	}
-	else
-	{
-		(*ptrNew_dog).owner = owner;
-	}
-
+	(*ptrNew_dog).owner = cpOwner;
 	return (ptrNew_dog);
 }
