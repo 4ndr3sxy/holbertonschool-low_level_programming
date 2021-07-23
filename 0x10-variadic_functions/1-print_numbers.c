91a0236de2622857ab1bafff750fb7023a0006e3
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-23 14:17:43 -0500

    [DEL] - Function main

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -30,10 +30,3 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_end(listArgs);
 	printf("\n");
 }
-
-
-int main(void)
-{
-    print_numbers(", ", 4, 0, 98, -1024, 402);
-    return (0);
-}
