7f3a82e18e12e6d93881cd121a9264920ff58a25
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-23 20:31:58 -0500

    [ADD] - Creation code and file task 4

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,35 @@
+; .model small
+; .stack 0100H
+; .data
+; string1 db 'Hello, Holberton', 40H, '$'
+; .code
+; mov ax, @data
+; mov ds, ax
+
+; lea dx, string1     ;load effective addres
+; mov ah, 9           ;write string to stdout
+; int 21H
+
+; mov ah, 4ch
+; int 21h
+; int 20h
+; end
+
+global _start
+
+section .text:
+
+_start:
+    mov eax, 0x4            ;use the write syscall
+    mov ebx, 1              ;use stdout as the fd
+    mov ecx, message        ;use de message as the buffer
+    mov edx, message_length ; and supply the length
+    int 0x80
+
+    mov eax, 0x1
+    mov ebx, 0
+    int 0x80
+
+section .data:
+    message: db "Hello, Holberton", 0xA
+    message_length equ $-message
