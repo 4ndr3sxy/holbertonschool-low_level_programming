a96a0c078b19fc44ec539f143dde74dee3356f04
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-07-30 12:41:09 -0500

    [FIX] - Fix struct

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,11 +11,11 @@
  * @len: size of str
  * @next: the next node
  */
-typedef struct list
+typedef struct list_s
 {
-	char *str;
-	int len;
-	struct list *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
