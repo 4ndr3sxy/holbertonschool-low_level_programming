738d0690520a479e579d384b4405d7a9beea594e
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-08-02 17:32:38 -0500

    [ADD] - Creation code and file task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - free mallocs
+ * @head: const with header of linked list.
+ * Return: Without Return.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp, *temp2;
+
+	temp2 = head;
+	while (temp2)
+	{
+		temp = temp2->next;
+		free(temp2);
+		temp2 = temp;
+	}
+}
