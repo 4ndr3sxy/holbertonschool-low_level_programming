b89b0f83d56272a965be73656029454fa81dc2a2
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-08-03 11:17:01 -0500

    [FIX] - Fix code with conditional temp == NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *temp = *head, *newNode, *temp2;
 
-	if (!*head)
+	if (!temp)
 		return (NULL);
 
 	for (i = 0; i < idx; i++)
