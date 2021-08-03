8da8d1f7b14187f8c6ba6f5c40eae08e4f91cf8b
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-08-03 11:15:35 -0500

    [FIX] - Fix code with conditional *head == NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,6 +12,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *temp = *head, *newNode, *temp2;
 
+	if (!*head)
+		return (NULL);
+
 	for (i = 0; i < idx; i++)
 	{
 		if (!temp)
