78fde1e21a360c8481fa23174992450f148d35a4
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-08-05 20:08:03 -0500

    [ADD] - Creation code and file task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * flip_bits - know the diferences of bits
+ * @n: number one
+ * @m: number two
+ * Return: Count of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int l = (n ^ m);
+	unsigned int count = 0;
+
+	while (l > 0)
+	{
+		count++;
+		l &= (l - 1);
+	}
+	return (count);
+}
