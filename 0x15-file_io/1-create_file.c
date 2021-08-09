9b2913cb272bc2054dc04b200212f8115996cb5d
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-08-09 16:25:36 -0500

    [FIX] - Fix in conditional na dvalues NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,8 +8,8 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, i = 0;
-	ssize_t val;
+	int file;
+	int val;
 
 	if (!filename)
 		return (-1);
@@ -17,16 +17,16 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
-	while (text_content[i] && text_content)
+	if (text_content)
 	{
-		i++;
+		val = write(file, text_content, strlen(text_content));
+		if (val == -1)
+		{
+			close(file);
+			return (-1);
+		}
 	}
 
-	val = write(file, text_content, (size_t)i);
-	if ((int)val == -1)
-		return (-1);
-
 	close(file);
-
 	return (1);
 }
