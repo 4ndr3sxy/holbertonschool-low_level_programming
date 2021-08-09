2b99bb0077e341862289815b75d4d8329f7212ee
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-08-09 15:24:20 -0500

    [FIX] - Fix in open(), truncated added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_CREAT, 0600);
+	file = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (file == -1)
 		return (-1);
 	while (text_content[i])
