1e3fc39bca2ca6467a5935747316ca9adcc425bc
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-08-09 15:50:27 -0500

    [ADD] - 0600 in open()

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_APPEND);
+	file = open(filename, O_WRONLY | O_APPEND, 0600);
 	if (file == -1)
 		return (-1);
 	while (text_content[i])
