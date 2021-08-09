b72fc7757cd0377666f4b808fbc7869260bb7dbf
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-08-09 15:32:16 -0500

    [FIX] - Fix in while()

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (file == -1)
 		return (-1);
-	while (text_content[i])
+	while (text_content[i] && text_content)
 	{
 		i++;
 	}
