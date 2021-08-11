378ed2df63935658afb89f0984caae7b1b1dd706
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-08-11 11:44:52 -0500

    [ADD] - Creation code and file task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,18 +10,34 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	if (*n1 == 0)
+	int i = 0, j = 0, sumTemp, higher, temp1, temp2, add = 0;
+
+	while (n1[i])
+		i++;
+	while (n2[j])
+		j++;
+	if (j >= i)
+		higher = j;
+	else
+		higher = i;
+	if (higher >= size_r - 1)
+		return (0);
+	r[higher + 1] = '\0';
+	i--, j--, size_r--;
+	temp1 = n1[i] - 48, temp2 = n2[j] - 48;
+	while (higher >= 0)
 	{
-
+		sumTemp = temp1 + temp2 + add;
+		add = (sumTemp >= 10 ? 1 : 0);
+		r[higher] = (sumTemp > 0 ? (sumTemp % 10) + 48 : '0');
+		i--;
+		j--;
+		temp1 = (i >= 0 ? n1[i] - 48 : 0);
+		temp2 = (j >= 0 ? n2[j] - 48 : 0);
+		higher--, size_r--;
 	}
-	if (*n2 == 0)
-	{
-
-	}
-	if (size_r)
-	{
-
-	}
-
-	return (r);
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
