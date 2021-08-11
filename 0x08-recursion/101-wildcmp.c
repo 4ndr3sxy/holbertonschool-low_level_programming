37df0626458aa7f4904757f7298e304a15e3778e
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-08-11 18:14:21 -0500

    [FIX] - Fix style betty

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 
 /**
- * is_palindrome - send the char * to function search_palindrome()
- * @s: Chain of characters
- * Return: 1 to palindrome or 0 to not palindrome.
+ * wildcmp - recursion function
+ * @s1: first chain of characters
+ * @s2: second chain of characters
+ * Return: 1 to sucess or 0 to failure.
  */
 int wildcmp(char *s1, char *s2)
 {
-	int val;
-
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s2 == '*')
@@ -38,36 +37,5 @@ int wildcmp(char *s1, char *s2)
 		else
 			return (wildcmp(s1 + 1, s2));
 	}
-	return (0);
-}
-
-int main(void)
-{
-	int r;
-
-	r = wildcmp("main.c", "*.c");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "m*a*i*n*.*c*");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "main.c");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "m*c");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "ma********************************c");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "*");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "***");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "m.*c");
-	printf("%d\n", r);
-	r = wildcmp("main.c", "**.*c");
-	printf("%d\n", r);
-	r = wildcmp("main-main.c", "ma*in.c");
-	printf("%d\n", r);
-	r = wildcmp("main", "main*d");
-	printf("%d\n", r);
-	r = wildcmp("abc", "*b");
-	printf("%d\n", r);
 	return (0);
 }
