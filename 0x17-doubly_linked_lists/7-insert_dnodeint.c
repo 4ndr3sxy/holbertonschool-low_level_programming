f41a085a5ed02b76764ec1a61a5ba096b464ff6a
Andres Castaneda <andrescastanedaxy@gmail.com>
2021-09-17 14:09:14 -0500

    [FIX] - Fix code in return *h NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!*h)
 	{
 		*h = newNode;
-		return (NULL);
+		return (newNode);
 	}
 	copyNode = *h;
 	copyCopyNode = copyNode;
