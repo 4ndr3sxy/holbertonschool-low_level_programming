420a5445dd21fc0e087e340ff983d45f97f7070c
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-09 12:37:09 -0500

    [DEL] - print in L.37 deleted

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (temp->key == (char *)key)
 		{
-			printf("Igualdad\n");
 			temp->value = (char *)value;
 			free(newNode);
 			val = 1;
