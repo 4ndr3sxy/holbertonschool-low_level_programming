067147fa35285485378f40e60fd3d22daca6e96b
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-09 12:43:29 -0500

    [ADD] - Creation code and file task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get a node with key
+ * @ht: hash table
+ * @key: key to get a node
+ * Return: Value in the key or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int valKey = 0;
+	hash_node_t *tempArray = NULL;
+
+	valKey = key_index((unsigned char *)key, ht->size);
+	if (ht->array[valKey])
+	{
+		tempArray = ht->array[valKey];
+		while (tempArray)
+		{
+			if (tempArray->key == key)
+				return (tempArray->value);
+			tempArray = tempArray->next;
+		}
+	}
+	return (NULL);
+}
