b33c9e53de53dca14c47d9383c724474a43f4ffa
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-09 12:22:23 -0500

    [ADD] - Creation code and file task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Create mallocs to hash table
+ * @size: size to array in struct hash_table_t
+ * Return: New struct type hash_table_t
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	hash_table_t *hashTable = NULL;
+
+	hashTable = malloc(sizeof(hash_table_t));
+	if (!hashTable)
+		return (NULL);
+
+	hashTable->array = malloc(sizeof(hash_node_t) * size);
+	if (!hashTable->array)
+		return (NULL);
+	hashTable->size = size;
+
+	for (; i < size; i++)
+		hashTable->array[i] = NULL;
+	return (hashTable);
+}
