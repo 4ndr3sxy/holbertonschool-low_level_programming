8293c12487cae2af857e04f5903dc51cb0e65aef
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-09 12:55:23 -0500

    [ADD] - Creation free to hashtable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,6 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hashTable->array = malloc(sizeof(hash_node_t) * size);
 	if (!hashTable->array)
+		free(hashTable);
 		return (NULL);
 
 	for (; i < size; i++)
